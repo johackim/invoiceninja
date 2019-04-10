ee076c9bef631828100042a3881537a54201973f
ston3o <contact@ston3o.me>
2019-04-10 12:35:39 +0200

    Fix cron

--- cron.sh
+++ cron.sh
@@ -4,7 +4,7 @@ chown www-data:www-data /var/www/app/public/logo/
 
 sleep 300s
 while /bin/true; do
-	/var/www/app/artisan ninja:send-invoices
-	/var/www/app/artisan ninja:send-reminders
+	/usr/local/bin/php /var/www/app/artisan ninja:send-invoices
+	/usr/local/bin/php /var/www/app/artisan ninja:send-reminders
 	sleep 1d
 done
